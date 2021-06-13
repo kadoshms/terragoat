41dfa6e88f8406ae9b671bd0ee6da1ae7ff7d47d
Mor Kadosh <kadoshms@gmail.com>
2021-06-13 09:47:59 +0300

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -59,3 +59,17 @@ resource "aws_s3_bucket" "test_bucket_supp_1" {
     Environment = local.resource_prefix.value
   }
 }
+
+resource "aws_s3_bucket" "some-test-run" {
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  arn           = "arn:aws:s3:::619572639823-acme-dev-financials-test-mor4"
+  bucket        = "${local.resource_prefix.value}-financials-test-mor4"
+  acl           = "private"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-financials-test-mor4"
+    Environment = local.resource_prefix.value
+  }
+}
